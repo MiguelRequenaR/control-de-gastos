73cb828706301a52ad4345349080e9ad8a72c41f
MiguelRequenaR <e_2019100844l@uncp.edu.pe>
2023-09-20 21:29:30 -0500

    add react swipeable list

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import Header from "./components/Header"
 import Modal from "./components/Modal";
 import ListadoGastos from "./components/ListadoGastos";
@@ -6,16 +6,27 @@ import {generarId} from "./helpers"
 import icono from "./img/nuevo-gasto.svg"
 
 function App() {
+  const [gastos, setGastos] = useState([])
   const [presupuesto, setPresupuesto] = useState(0);
   const [isValidPresupuesto, setIsValidPresupuesto] = useState(false);
 
   const [modal, setModal] = useState(false)
   const [animarModal, setAnimarModal] = useState(false)
-  const [gastos, setGastos] = useState([])
+
+  const [gastoEditar, setGastoEditar] = useState({})
+
+  useEffect(() => {
+    if(Object.keys(gastoEditar).length > 0){
+      setModal(true)
+      setTimeout(() => {
+        setAnimarModal(true)
+      }, 100);
+    }
+  }, [gastoEditar])
 
   const handleNuevoGasto = () => {
     setModal(true)
-
+    setGastoEditar({})
     setTimeout(() => {
       setAnimarModal(true)
     }, 100);
@@ -35,8 +46,9 @@ function App() {
   }
 
   return (
-    <>
+    <div className={modal ? 'fijar' : ''}>
       <Header
+          gastos = {gastos}
           presupuesto = {presupuesto}
           setPresupuesto = {setPresupuesto}
           isValidPresupuesto = {isValidPresupuesto}
@@ -47,6 +59,7 @@ function App() {
           <main>
               <ListadoGastos
                   gastos = {gastos}
+                  setGastoEditar = {setGastoEditar}
               />
           </main>
           <div className="nuevo-gasto">
@@ -63,8 +76,9 @@ function App() {
           animarModal = {animarModal}
           setAnimarModal = {setAnimarModal}
           guardarGasto = {guardarGasto}
+          gastoEditar = {gastoEditar}
         />}
-    </>
+    </div>
   )
 }
 
--- src/components/Gasto.jsx
+++ src/components/Gasto.jsx
@@ -1,28 +1,79 @@
-import {formatearFecha} from '../helpers'
+import {
+    LeadingActions,
+    SwipeableList,
+    SwipeableListItem,
+    SwipeAction,
+    TrailingActions
+} from 'react-swipeable-list';
+import "react-swipeable-list/dist/styles.css";
+import {formatearFecha} from '../helpers';
 
-const Gasto = ({gasto}) => {
+import icono1 from '../img/icono_ahorro.svg'
+import icono2 from '../img/icono_casa.svg'
+import icono3 from '../img/icono_comida.svg'
+import icono4 from '../img/icono_gastos.svg'
+import icono5 from '../img/icono_ocio.svg'
+import icono6 from '../img/icono_salud.svg'
+import icono7 from '../img/icono_suscripciones.svg'
+
+const dicIcon = {
+    ahorro: icono1,
+    casa: icono2,
+    comida: icono3,
+    gastos: icono4,
+    ocio: icono5,
+    salud: icono6,
+    suscripciones: icono7,
+}
+
+const Gasto = ({gasto, setGastoEditar}) => {
     const {categoria, nombre, cantidad, id, fecha} = gasto;
 
+    const leadingActions = () => (
+        <LeadingActions>
+            <SwipeAction onClick={() => setGastoEditar(gasto)}>
+                Editar
+            </SwipeAction>
+        </LeadingActions>
+    )
+    const trailingActions = () => (
+        <TrailingActions>
+            <SwipeAction onClick={() => console.log('Eliminar')}>
+                Eliminar
+            </SwipeAction>
+        </TrailingActions>
+    )
   return (
-    <div className="gasto sombra">
-        <div className="contenido-gasto">
-            <div className="descripcion-gasto">
-                <p className="categoria">
-                    {categoria}
-                </p>
-                <p className="nombre-gasto">
-                    {nombre}
-                </p>
-                <p className="fecha-gasto">
-                    Agregado el: {''}
-                    <span>{formatearFecha(fecha)}</span>
-                </p>
+    <SwipeableList>
+        <SwipeableListItem
+            leadingActions = {leadingActions()}
+            trailingActions = {trailingActions()}
+        >
+            <div className="gasto sombra">
+                <div className="contenido-gasto">
+                    <img 
+                        src={dicIcon[categoria]}
+                        alt='Icono Gasto'
+                    />
+                    <div className="descripcion-gasto">
+                        <p className="categoria">
+                            {categoria}
+                        </p>
+                        <p className="nombre-gasto">
+                            {nombre}
+                        </p>
+                        <p className="fecha-gasto">
+                            Agregado el: {''}
+                            <span>{formatearFecha(fecha)}</span>
+                        </p>
+                    </div>
+                </div>
+                    <p className='cantidad-gasto'>
+                        ${cantidad}
+                    </p>
             </div>
-        </div>
-            <p className='cantidad-gasto'>
-                ${cantidad}
-            </p>
-    </div>
+        </SwipeableListItem>
+    </SwipeableList>
   )
 }
 
